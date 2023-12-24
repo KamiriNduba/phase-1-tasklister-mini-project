d2134b2d41a650d52c52ebe24c61c49df38c766c
NDUBA <user@users-MacBook-Pro.local>
2023-12-24 23:46:55 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent default form submission behavior
+
+    const taskInput = document.getElementById("new-task-description");
+    const taskText = taskInput.value;
+
+    if (taskText.trim() !== "") {
+      const newTask = document.createElement("li");
+      newTask.textContent = taskText;
+
+      taskList.appendChild(newTask);
+      taskInput.value = ""; // Clear input field after task is added
+    }
+  });
 });
